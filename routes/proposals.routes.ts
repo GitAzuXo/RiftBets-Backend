5211a84adffc55c0e494388f4012e5004dc3f41e
Louis Hislaire <122865223+GitAzuXo@users.noreply.github.com>
2025-05-17 00:09:02 +0200

    Update proposals.routes.ts

--- routes/proposals.routes.ts
+++ routes/proposals.routes.ts
@@ -13,8 +13,8 @@ router.get("/getAll", (req, res) => {
         COALESCE(SUM(b.bet_amount), 0) AS prop_total_amount,
         COUNT(DISTINCT b.bet_user) AS prop_nbPeople
     FROM proposals p
-    WHERE p.prop_available = 1
     LEFT JOIN bet b ON p.prop_id = b.bet_proposal
+    WHERE p.prop_available = 1
     GROUP BY p.prop_id, p.prop_player, p.prop_title, p.prop_odds, p.prop_creation, p.prop_available;`;
     db.query<RowDataPacket[]>(sql).then(([results]) => {
         if (results.length === 0) {
